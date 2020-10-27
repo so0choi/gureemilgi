bd54d208eba15a6771ae7dfad54efe3cb46c5393
so0choi <simc2644@gmail.com>
2020-10-27 12:23:02 +0900

    add db connection in signup controller

--- controllers/user/signup/signup.ctrl.js
+++ controllers/user/signup/signup.ctrl.js
@@ -4,11 +4,25 @@ exports.get_signup = (req, res) => {
   res.render("register.html");
 };
 
-exports.post_signup = (req, res) => {
-  const id = req.body.id;
-  console.log(id);
-  getConnection((conn) => {
-    console.log("hi");
-    conn.release();
+function createUser(id, pwd, name) {
+  return new Promise((resolve, reject) => {
+    let sql = `INSERT INTO USER(id, pwd, name) VALUES ('${id}','${pwd}','${name}');`;
+    console.log(sql);
+    getConnection((conn) => {
+      conn.query(sql, (err, result) => {
+        conn.release();
+        if (err) reject(err);
+        resolve(true);
+      });
+    });
   });
+}
+
+exports.post_signup = (req, res) => {
+  console.log(req.body);
+  const { id, pwd, name } = req.body;
+  if (createUser(id, pwd, name)) {
+    console.log("success");
+    res.render("register.html");
+  }
 };
